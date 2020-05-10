cc64ffa706f519e560fa5a41c157a4554a5596d1
陈生水 <chenshengshui@cvte.com>
2020-05-10 15:12:57 +0800

    chore: 增加预发布脚本

---
+++ release.sh
@@ -0,0 +1,25 @@
+```
+#!/bin/bash
+
+# 可以直接设置为开发分支，origin为远程地址
+master="master"
+origin="git@github.com:chenshengshui/happy-music.git" 
+
+git fetch $origin
+echo "Current fetch all Tags"
+
+git pull $origin $master
+echo "Current pull origin $master."
+
+# 自动生成tag和修改版本号
+npm version patch
+conventional-changelog -p eslint -i CHANGELOG.md -s -r 0
+
+git add CHANGELOG.md
+git commit -m "docs: update changelog"
+
+git push --follow-tags origin $master
+
+echo "Git push origin $master"
+echo "Release finished."
+```
